13e21810224741b57c853ca1aa26203a73584e1d
Piotr Kantor <piotr.kantor@gmail.com>
2017-03-16 10:41:49 +0100

    Adding more mockito examples

--- src/test/java/MockTest/FooTest.java
+++ src/test/java/MockTest/FooTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 
 import static MockException.Foo.HELLO_WORLD;
 import static org.junit.Assert.assertEquals;
+//import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 
 public class FooTest {
 
@@ -24,15 +28,40 @@ public class FooTest {
     }
 
     @Test
-    public void shouldRetunWhatIWant(){
+    public void shouldRetunParameterWithAdditionalMessage(){
         //given
         Foo foo=mock(Foo.class);
         when(foo.greet(HELLO_WORLD)).thenReturn(HELLO_WORLD+" To nieprawidłowe hasło :D");
 
         //when
-        System.out.println("Foo greets: " + foo.greet());
+        System.out.println("Foo greets: " + foo.greet(HELLO_WORLD));
 
         //then
-        assertEquals(foo.greet(), "Hello World To nieprawidłowe hasło :D");
+        assertEquals("Hello World To nieprawidłowe hasło :D", foo.greet(HELLO_WORLD));
     }
+
+    @Test
+    public void shouldReturnCustomMessageForAnyGivenMessage(){
+        //given
+        Foo foo=mock(Foo.class);
+        when(foo.greet(anyString())).thenReturn("Custom Message");
+
+        //then
+        assertEquals("Custom Message", foo.greet("Jakiś napis"));
+        assertEquals("Custom Message", foo.greet("Jakiś zupełnie inny napis"));
+
+    }
+
+    @Test
+    public void shouldReturnFirstParameter(){
+        //given
+        Foo foo=mock(Foo.class);
+        when(foo.greet(anyString())).then(returnsFirstArg());
+
+        //then
+        assertEquals("Jakiś napis", foo.greet("Jakiś napis"));
+        assertEquals("Jakiś zupełnie inny napis", foo.greet("Jakiś zupełnie inny napis"));
+    }
+
+
 }
